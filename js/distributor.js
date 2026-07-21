e309a33fc6ac6d87f4e73a7a04fd0e9c78aed31b
CStathakis95 <cstathakis@uniting.org>
2026-07-21 16:57:12 +1000

    Update distributor.js

--- js/distributor.js
+++ js/distributor.js
@@ -1,23 +1,51 @@
-// Distributor detection will go here
 function findDistributor(address) {
 
     address = address.toLowerCase();
 
-    if (
-        address.includes("sydney") ||
-        address.includes("newcastle") ||
-        address.includes("central coast")
-    ) {
-        return "Ausgrid";
+
+    const endeavourAreas = [
+        "penrith",
+        "blackett",
+        "st marys",
+        "mount druitt",
+        "doonside",
+        "whalan",
+        "plumpton",
+        "rooty hill",
+        "parramatta",
+        "seven hills",
+        "werrington",
+        "kingswood",
+        "cranebrook",
+        "emu plains",
+        "springwood",
+        "wollongong"
+    ];
+
+
+    const ausgridAreas = [
+        "sydney",
+        "newcastle",
+        "central coast",
+        "hornsby",
+        "manly",
+        "parramatta"
+    ];
+
+
+    for (const area of endeavourAreas) {
+        if (address.includes(area)) {
+            return "Endeavour Energy";
+        }
     }
 
-    if (
-        address.includes("penrith") ||
-        address.includes("parramatta") ||
-        address.includes("wollongong")
-    ) {
-        return "Endeavour Energy";
+
+    for (const area of ausgridAreas) {
+        if (address.includes(area)) {
+            return "Ausgrid";
+        }
     }
+
 
     return "Essential Energy";
 
