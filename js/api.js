c9d56cb5baa39f8b3f1b8e8cc0a4e374933e2610
CStathakis95 <cstathakis@uniting.org>
2026-08-11 11:02:57 +1000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,42 +1,29 @@
 async function getOutage(address) {
-
-```
-const response = await fetch(
-    `${CONFIG.API_URL}/check`,
-    {
-        method: "POST",
-
-        headers: {
-            "Content-Type": "application/json"
-        },
-
-        body: JSON.stringify({
-            address: address
-        })
-    }
-);
-
-
-if (!response.ok) {
-
-    throw new Error(
-        `Cloudflare returned HTTP ${response.status}`
+    const response = await fetch(
+        `${CONFIG.API_URL}/check`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                address: address
+            })
+        }
     );
 
-}
+    if (!response.ok) {
+        throw new Error(
+            `Cloudflare returned HTTP ${response.status}`
+        );
+    }
 
+    const data = await response.json();
 
-const data =
-    await response.json();
+    console.log(
+        "Worker response:",
+        data
+    );
 
-
-console.log(
-    "Worker response:",
-    data
-);
-
-
-return data;
-```
-
+    return data;
 }
