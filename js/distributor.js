770fed53a1293e399cd03752fb7ee776b1103ee8
CStathakis95 <cstathakis@uniting.org>
2026-08-11 11:43:56 +1000

    Update distributor.js

--- js/distributor.js
+++ js/distributor.js
@@ -1,27 +1,25 @@
 /*
-
-* Distributor identification
-*
-* This module will eventually determine whether an address
-* is supplied by:
-*
-* * Ausgrid
-* * Endeavour Energy
-* * Essential Energy
-*
-* We are deliberately NOT using suburb-name guessing here.
-  */
+ * Distributor identification
+ *
+ * This module will eventually determine whether an address
+ * is supplied by:
+ *
+ * - Ausgrid
+ * - Endeavour Energy
+ * - Essential Energy
+ *
+ * We are deliberately NOT using suburb-name guessing here.
+ */
 
 function findDistributor(address) {
 
-console.log(
-    "Distributor lookup requested for:",
-    address
-);
+    console.log(
+        "Distributor lookup requested for:",
+        address
+    );
 
-return {
-    name: "Not yet determined",
-    status: "Distributor boundary lookup not connected"
-};
-
+    return {
+        name: "Not yet determined",
+        status: "Distributor boundary lookup not connected"
+    };
 }
